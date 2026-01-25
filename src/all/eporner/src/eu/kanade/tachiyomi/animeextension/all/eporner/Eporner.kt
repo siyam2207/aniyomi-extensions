feaafae4c4cb88c3ed2e456c813bd0ee13415526
siyam2207 <siyambackupfile@gmail.com>
2026-01-25 14:31:49 +0600

    Update Eporner.kt

--- src/all/eporner/src/eu/kanade/tachiyomi/animeextension/all/eporner/Eporner.kt
+++ src/all/eporner/src/eu/kanade/tachiyomi/animeextension/all/eporner/Eporner.kt
@@ -1,133 +1,91 @@
-package eu.kanade.tachiyomi.animesource.all.eporner
+package eu.kanade.tachiyomi.animeextension.all.eporner
 
-import eu.kanade.tachiyomi.animesource.model.SAnime
-import eu.kanade.tachiyomi.animesource.model.SEpisode
-import eu.kanade.tachiyomi.animesource.model.Video
-import eu.kanade.tachiyomi.animesource.online.ParsedHttpSource
-import eu.kanade.tachiyomi.network.GET
-import okhttp3.Headers
+import eu.kanade.tachiyomi.animesource.model.*
+import eu.kanade.tachiyomi.animesource.online.AnimeHttpSource
 import okhttp3.OkHttpClient
+import okhttp3.Headers
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
+import eu.kanade.tachiyomi.network.GET
 
-class Eporner : ParsedHttpSource() {
+class Eporner : AnimeHttpSource() {
 
     override val name = "Eporner"
     override val baseUrl = "https://www.eporner.com"
     override val lang = "all"
     override val supportsLatest = true
 
-    override fun client(): OkHttpClient = network.client
+    override val client: OkHttpClient = OkHttpClient()
 
     // -------------------
-    // Popular Videos
+    // Popular
     // -------------------
     override fun popularAnimeRequest(page: Int) =
-        GET(
-            "$baseUrl/videos/?o=mv&p=$page",
-        )
+        GET("$baseUrl/videos/?o=mv&p=$page")
 
-    override fun popularAnimeSelector(): String = "a.videoBox"
+    override fun popularAnimeParse(response: okhttp3.Response): AnimesPage {
+        val document = response.asJsoup()
+        val elements = document.select("a.videoBox")
+        val animeList = elements.map { popularAnimeFromElement(it) }
+        val hasNextPage = document.selectFirst("a.next, a[rel=next]") != null
+        return AnimesPage(animeList, hasNextPage)
+    }
 
-    override fun popularAnimeFromElement(element: Element): SAnime {
+    private fun popularAnimeFromElement(element: Element): SAnime {
         return SAnime.create().apply {
             title = element.selectFirst("span.title")?.text() ?: "Unknown"
             thumbnail_url =
-                element.selectFirst("img")
-                    ?.attr("data-src")
+                element.selectFirst("img")?.attr("data-src")
                     ?.takeIf { it.isNotBlank() }
                     ?: element.selectFirst("img")?.attr("src")
             url = element.attr("href")
         }
     }
 
-    override fun popularAnimeNextPageSelector(): String =
-        "a.next, a[rel=next]"
-
     // -------------------
-    // Latest Videos
+    // Latest
     // -------------------
     override fun latestUpdatesRequest(page: Int) =
-        GET(
-            "$baseUrl/videos/?o=mv&p=$page",
-        )
+        GET("$baseUrl/videos/?o=mv&p=$page")
 
-    override fun latestUpdatesSelector(): String = "a.videoBox"
-
-    override fun latestUpdatesFromElement(element: Element): SAnime =
-        popularAnimeFromElement(element)
-
-    override fun latestUpdatesNextPageSelector(): String =
-        "a.next, a[rel=next]"
+    override fun latestUpdatesParse(response: okhttp3.Response): AnimesPage =
+        popularAnimeParse(response)
 
     // -------------------
     // Search
     // -------------------
     override fun searchAnimeRequest(page: Int, query: String) =
-        GET(
-            "$baseUrl/search/${query.replace(" ", "+")}/?o=mv&p=$page",
-        )
+        GET("$baseUrl/search/${query.replace(" ", "+")}/?o=mv&p=$page")
 
-    override fun searchAnimeSelector(): String = "a.videoBox"
-
-    override fun searchAnimeFromElement(element: Element): SAnime =
-        popularAnimeFromElement(element)
-
-    override fun searchAnimeNextPageSelector(): String =
-        "a.next, a[rel=next]"
+    override fun searchAnimeParse(response: okhttp3.Response): AnimesPage =
+        popularAnimeParse(response)
 
     // -------------------
-    // Episode List
+    // Episodes
     // -------------------
-    override fun episodeListParse(document: Document): List<SEpisode> {
-        return listOf(
-            SEpisode.create().apply {
-                name = "Video"
-                url = document.location()
-            },
-        )
+    override fun episodeListParse(response: okhttp3.Response): List<SEpisode> {
+        return listOf(SEpisode.create().apply {
+            name = "Video"
+            url = response.request.url.toString()
+        })
     }
 
-    override fun episodeListSelector(): String = ""
-
-    override fun episodeFromElement(element: Element): SEpisode =
-        SEpisode.create()
-
     // -------------------
-    // Video Playback
+    // Video
     // -------------------
-    override fun videoListParse(document: Document): List<Video> {
-        val regex =
-            Regex(
-                """https://dash[^"]+\.mp4\.urlset/master\.m3u8[^"]*""",
-            )
-
-        val hlsUrl =
-            regex.find(document.html())?.value
-                ?: error("HLS master playlist not found")
+    override fun videoListParse(response: okhttp3.Response): List<Video> {
+        val document = response.asJsoup()
+        val regex = Regex("""https://dash[^"]+\.mp4\.urlset/master\.m3u8[^"]*""")
+        val hlsUrl = regex.find(document.html())?.value
+            ?: throw Exception("HLS master playlist not found")
 
         return listOf(
             Video(
                 url = hlsUrl,
                 quality = "HLS",
                 videoUrl = hlsUrl,
-                headers = Headers.headersOf(
-                    "Referer",
-                    baseUrl,
-                ),
-            ),
+                headers = Headers.headersOf("Referer", baseUrl)
+            )
         )
     }
-
-    override fun videoListSelector(): String = ""
-
-    override fun videoFromElement(element: Element): Video =
-        Video(
-            url = "",
-            quality = "",
-            videoUrl = "",
-            headers = Headers.headersOf(),
-        )
-
-    override fun videoUrlParse(document: Document): String = ""
 }
