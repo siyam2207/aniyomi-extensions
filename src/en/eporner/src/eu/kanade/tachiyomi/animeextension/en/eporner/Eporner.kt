9554b0b9733b77158abb5112c4c5455cbf264ea2
siyam2207 <siyambackupfile@gmail.com>
2026-01-29 15:20:24 +0600

    Update Eporner.kt

--- src/en/eporner/src/eu/kanade/tachiyomi/animeextension/en/eporner/Eporner.kt
+++ src/en/eporner/src/eu/kanade/tachiyomi/animeextension/en/eporner/Eporner.kt
@@ -1,14 +1,20 @@
 package eu.kanade.tachiyomi.animeextension.en.eporner
 
 import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
+import eu.kanade.tachiyomi.animesource.model.AnimesPage
 import eu.kanade.tachiyomi.animesource.model.SAnime
 import eu.kanade.tachiyomi.animesource.model.SEpisode
 import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.animesource.online.AnimeHttpSource
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.util.asJsoup
+import okhttp3.Headers
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
-import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import java.lang.Exception
 
 class Eporner : AnimeHttpSource() {
 
@@ -18,79 +24,105 @@ class Eporner : AnimeHttpSource() {
     override val supportsLatest: Boolean = true
     override val client: OkHttpClient = network.cloudflareClient
 
-    // --------------------------- Popular ---------------------------
-
-    override fun popularAnimeRequest(page: Int): Request {
-        val url = "$baseUrl/popular/?page=$page"
-        return GET(url, headers)
+    // --------------------------- Latest ---------------------------
+    override fun latestUpdatesRequest(page: Int): Request {
+        return GET("$baseUrl/most-recent/$page/", headers)
     }
 
-    override fun popularAnimeParse(response: Response): List<SAnime> {
-        val document = Jsoup.parse(response.body!!.string())
-        return document.select("div.video-block").map { element ->
-            SAnime.create().apply {
-                title = element.select("a.title").text()
-                setUrlWithoutDomain(element.select("a.title").attr("href"))
-                thumbnail_url = element.select("img").attr("src")
-            }
+    override fun latestUpdatesParse(response: Response): AnimesPage {
+        return parseAnimeList(response)
+    }
+
+    // --------------------------- Popular ---------------------------
+    override fun popularAnimeRequest(page: Int): Request {
+        return GET("$baseUrl/most-viewed/$page/", headers)
+    }
+
+    override fun popularAnimeParse(response: Response): AnimesPage {
+        return parseAnimeList(response)
+    }
+
+    // --------------------------- Search ---------------------------
+    override fun searchAnimeRequest(page: Int, query: String, filters: AnimeFilterList): Request {
+        return GET("$baseUrl/search/$query/$page/", headers)
+    }
+
+    override fun searchAnimeParse(response: Response): AnimesPage {
+        return parseAnimeList(response)
+    }
+
+    // --------------------------- Anime Details ---------------------------
+    override fun animeDetailsParse(response: Response): SAnime {
+        val document = response.asJsoup()
+        return SAnime.create().apply {
+            title = document.select("h1.title").text()
+            thumbnail_url = document.select("meta[property=og:image]").attr("content")
+            description = document.select("div.video-description").text()
+            genre = document.select("div.categories a").joinToString(", ") { it.text() }
         }
     }
 
     // --------------------------- Episodes ---------------------------
-
-    override fun episodeListRequest(anime: SAnime): Request {
-        return GET(baseUrl + anime.url, headers)
-    }
-
     override fun episodeListParse(response: Response): List<SEpisode> {
-        val document = Jsoup.parse(response.body!!.string())
-        return document.select("ul.video-list li").map { element ->
-            SEpisode.create().apply {
-                name = element.select("a.title").text()
-                setUrlWithoutDomain(element.select("a").attr("href"))
-            }
-        }
+        // For this type of site, usually each video is its own "episode"
+        val document = response.asJsoup()
+        return listOf(SEpisode.create().apply {
+            name = "Watch"
+            episode_number = 1F
+            setUrlWithoutDomain(response.request.url.toString())
+        })
     }
 
     // --------------------------- Video Links ---------------------------
+    override fun videoListParse(response: Response): List<Video> {
+        val document = response.asJsoup()
+        val videos = mutableListOf<Video>()
+        
+        // Try to extract video URLs from the page
+        val scriptText = document.select("script").find { 
+            it.html().contains("video_url") || it.html().contains("mp4") 
+        }?.html() ?: ""
+        
+        // Look for mp4 URLs in the script
+        val mp4Pattern = Regex("""(https?://[^"'\s]+\.mp4[^"'\s]*)""")
+        val matches = mp4Pattern.findAll(scriptText)
+        
+        matches.forEach { match ->
+            val url = match.value
+            val quality = when {
+                url.contains("1080") -> "1080p"
+                url.contains("720") -> "720p"
+                url.contains("480") -> "480p"
+                url.contains("360") -> "360p"
+                else -> "Unknown"
+            }
+            videos.add(Video(url, quality, url))
+        }
+        
+        return videos
+    }
 
     override fun videoListRequest(episode: SEpisode): Request {
         return GET(baseUrl + episode.url, headers)
     }
 
-    override fun videoListParse(response: Response): List<Video> {
-        val document = Jsoup.parse(response.body!!.string())
-        val videos = mutableListOf<Video>()
-        document.select("source[type=video/mp4]").forEach { element ->
-            val url = element.attr("src")
-            val quality = element.attr("res") ?: "HD"
-            videos.add(Video(url, quality, url))
-        }
-        return videos
-    }
-
     // --------------------------- Helpers ---------------------------
-
-    private fun GET(url: String, headers: Map<String, String> = emptyMap()): Request {
-        return Request.Builder()
-            .url(url)
-            .headers(okhttp3.Headers.headersOf(*headers.flatMap { listOf(it.key, it.value) }.toTypedArray()))
-            .build()
-    }
-
-    override fun searchAnimeRequest(page: Int, query: String, filters: AnimeFilterList): Request {
-        val url = "$baseUrl/search/?q=$query&page=$page"
-        return GET(url, headers)
-    }
-
-    override fun searchAnimeParse(response: Response): List<SAnime> {
-        val document = Jsoup.parse(response.body!!.string())
-        return document.select("div.video-block").map { element ->
+    private fun parseAnimeList(response: Response): AnimesPage {
+        val document = response.asJsoup()
+        val animeList = document.select("div.mb h3 a").map { element ->
             SAnime.create().apply {
-                title = element.select("a.title").text()
-                setUrlWithoutDomain(element.select("a").attr("href"))
-                thumbnail_url = element.select("img").attr("src")
+                title = element.text()
+                setUrlWithoutDomain(element.attr("href"))
+                thumbnail_url = element.parent()?.parent()?.select("img")?.attr("src") ?: ""
             }
         }
+        
+        val hasNextPage = document.select("a.next").isNotEmpty()
+        return AnimesPage(animeList, hasNextPage)
+    }
+
+    // --------------------------- Required but not used ---------------------------
+    override fun getFilterList(): AnimeFilterList {
+        return AnimeFilterList()
     }
 }
