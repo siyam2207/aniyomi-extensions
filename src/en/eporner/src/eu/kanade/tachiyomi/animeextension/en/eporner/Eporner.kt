2fae1d91bd38b0bc979640fc20e19c474d4026de
siyam2207 <siyambackupfile@gmail.com>
2026-01-29 15:14:34 +0600

    Update Eporner.kt

--- src/en/eporner/src/eu/kanade/tachiyomi/animeextension/en/eporner/Eporner.kt
+++ src/en/eporner/src/eu/kanade/tachiyomi/animeextension/en/eporner/Eporner.kt
@@ -9,9 +9,6 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
-import org.jsoup.nodes.Element
-import java.lang.Exception
 
 class Eporner : AnimeHttpSource() {
 
