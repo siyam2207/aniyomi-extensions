3c2be91afcd30c1cadcf5c92026341e7d8c5f626
siyam2207 <siyambackupfile@gmail.com>
2026-02-02 09:56:37 +0600

    Fix video name generation in HLS extraction

--- src/all/eporner/src/eu/kanade/tachiyomi/animeextension/all/eporner/EpornerVideoExtractor.kt
+++ src/all/eporner/src/eu/kanade/tachiyomi/animeextension/all/eporner/EpornerVideoExtractor.kt
@@ -49,8 +49,8 @@ internal class EpornerVideoExtractor(
                                     playlistUtils.extractFromHls(
                                         url,
                                         response.request.url.toString(),
-                                        videoNameGen = { q -> "HLS - $q" }
-                                    )
+                                        videoNameGen = { q -> "HLS - $q" },
+                                    ),
                                 )
                             } catch (e: Exception) {
                                 Log.e(tag, "HLS extraction failed: ${e.message}")
