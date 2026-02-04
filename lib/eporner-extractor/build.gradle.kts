142133e9f5d30edee3f9630ddaa10db5447a8290
siyam2207 <siyambackupfile@gmail.com>
2026-02-04 18:43:11 +0600

    Update build.gradle.kts

--- lib/eporner-extractor/build.gradle.kts
+++ lib/eporner-extractor/build.gradle.kts
@@ -3,14 +3,5 @@ plugins {
 }
 
 dependencies {
-    // Optional JSON parsing if you need later
-    implementation(libs.jsunpacker) {
-        exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk8")
-    }
-
-    // Required for HLS / DASH parsing
     implementation(project(":lib:playlist-utils"))
-
-    // If you need shared utilities like synchrony
-    // implementation(project(":lib:synchrony"))
 }
