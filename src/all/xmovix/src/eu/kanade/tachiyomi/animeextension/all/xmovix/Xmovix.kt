5aafc1d1deaf80a57f7facaaee559fbac7e635e2
siyam2207 <siyambackupfile@gmail.com>
2026-02-12 15:34:24 +0600

    Update Xmovix.kt

--- src/all/xmovix/src/eu/kanade/tachiyomi/animeextension/all/xmovix/Xmovix.kt
+++ src/all/xmovix/src/eu/kanade/tachiyomi/animeextension/all/xmovix/Xmovix.kt
@@ -1,9 +1,13 @@
 package eu.kanade.tachiyomi.animeextension.all.xmovix
 
+import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
+import eu.kanade.tachiyomi.animesource.model.SAnime
+import eu.kanade.tachiyomi.animesource.model.SEpisode
+import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.animesource.online.ParsedAnimeHttpSource
-import eu.kanade.tachiyomi.animesource.model.*
 import eu.kanade.tachiyomi.network.GET
 import okhttp3.Request
+import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 
 class Xmovix : ParsedAnimeHttpSource() {
@@ -20,34 +24,43 @@ class Xmovix : ParsedAnimeHttpSource() {
     override fun popularAnimeRequest(page: Int): Request =
         GET("$baseUrl/en?page=$page", headers)
 
-    override fun popularAnimeSelector() = "div.movie-item"
+    override fun popularAnimeSelector(): String =
+        "div.movie-item"
 
     override fun popularAnimeFromElement(element: Element): SAnime {
         val anime = SAnime.create()
-        anime.title = element.select("h3.title").text()
+
+        anime.title =
+            element.select("h3.title").text()
+
         anime.setUrlWithoutDomain(
-            element.select("a").attr("href")
+            element.selectFirst("a")!!.attr("href")
         )
+
         anime.thumbnail_url =
-            element.select("img").attr("src")
+            element.selectFirst("img")!!.attr("src")
+
         return anime
     }
 
-    override fun popularAnimeNextPageSelector() = "a.next"
+    override fun popularAnimeNextPageSelector(): String? =
+        "a.next"
 
     // ==============================
     // Details
     // ==============================
 
-    override fun animeDetailsParse(document: org.jsoup.nodes.Document): SAnime {
+    override fun animeDetailsParse(document: Document): SAnime {
         val anime = SAnime.create()
 
-        anime.title = document.select("h1").text()
+        anime.title =
+            document.selectFirst("h1")!!.text()
+
         anime.thumbnail_url =
-            document.select("div.poster img").attr("src")
+            document.selectFirst("div.poster img")!!.attr("src")
 
         anime.description =
-            document.select("div.description").text()
+            document.selectFirst("div.description")?.text()
 
         return anime
     }
@@ -56,33 +69,38 @@ class Xmovix : ParsedAnimeHttpSource() {
     // Episodes
     // ==============================
 
-    override fun episodeListSelector() = "ul.episodes li"
+    override fun episodeListSelector(): String =
+        "ul.episodes li"
 
     override fun episodeFromElement(element: Element): SEpisode {
-        val ep = SEpisode.create()
+        val episode = SEpisode.create()
 
-        ep.name = element.select("a").text()
-        ep.setUrlWithoutDomain(
-            element.select("a").attr("href")
+        episode.name =
+            element.selectFirst("a")!!.text()
+
+        episode.setUrlWithoutDomain(
+            element.selectFirst("a")!!.attr("href")
         )
 
-        return ep
+        return episode
     }
 
     // ==============================
-    // Video links
+    // Videos
     // ==============================
 
-    override fun videoListParse(
-        document: org.jsoup.nodes.Document
-    ): List<Video> {
-
+    override fun videoListParse(document: Document): List<Video> {
         val videos = mutableListOf<Video>()
 
         document.select("video source").forEach {
             val url = it.attr("src")
+
             videos.add(
-                Video(url, "Default", url)
+                Video(
+                    url = url,
+                    quality = "Default",
+                    videoUrl = url,
+                ),
             )
         }
 
@@ -98,12 +116,20 @@ class Xmovix : ParsedAnimeHttpSource() {
         query: String,
         filters: AnimeFilterList
     ): Request {
-        return GET("$baseUrl/en/search/$query?page=$page", headers)
+        return GET(
+            "$baseUrl/en/search/$query?page=$page",
+            headers,
+        )
     }
 
-    override fun searchAnimeSelector() = popularAnimeSelector()
-    override fun searchAnimeFromElement(element: Element) =
+    override fun searchAnimeSelector(): String =
+        popularAnimeSelector()
+
+    override fun searchAnimeFromElement(
+        element: Element
+    ): SAnime =
         popularAnimeFromElement(element)
-    override fun searchAnimeNextPageSelector() =
+
+    override fun searchAnimeNextPageSelector(): String? =
         popularAnimeNextPageSelector()
 }
