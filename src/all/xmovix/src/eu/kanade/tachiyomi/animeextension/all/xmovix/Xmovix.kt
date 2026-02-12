c9cef0666849816cb997bf9efab5f2fa8dcc70de
siyam2207 <siyambackupfile@gmail.com>
2026-02-12 18:13:45 +0600

    Update Xmovix.kt

--- src/all/xmovix/src/eu/kanade/tachiyomi/animeextension/all/xmovix/Xmovix.kt
+++ src/all/xmovix/src/eu/kanade/tachiyomi/animeextension/all/xmovix/Xmovix.kt
@@ -10,7 +10,6 @@ import eu.kanade.tachiyomi.network.GET
 import okhttp3.Request
 import okhttp3.Response
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 
 class Xmovix : AnimeHttpSource() {
