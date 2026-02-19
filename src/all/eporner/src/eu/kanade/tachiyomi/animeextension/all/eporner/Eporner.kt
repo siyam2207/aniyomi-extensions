57fb66d93510fda9592302a005f187a555c62401
siyam2207 <siyambackupfile@gmail.com>
2026-02-19 21:17:21 +0600

    Update Eporner.kt

--- src/all/eporner/src/eu/kanade/tachiyomi/animeextension/all/eporner/Eporner.kt
+++ src/all/eporner/src/eu/kanade/tachiyomi/animeextension/all/eporner/Eporner.kt
@@ -18,7 +18,6 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonArray
-import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
 import okhttp3.Headers
